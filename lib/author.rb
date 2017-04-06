e1968f16ec2afbe37f9c7274af2458f93b46ce56
github username <github email address>
2017-04-06 17:06:54 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Author
+  attr_accessor :name
+
+  def author
+    author = Author.new
+    author
+  end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+  attr_accessor :title, :author
+
+  def post
+    post = Post.new
+    post
+  end
+end
